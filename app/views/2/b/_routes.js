afaead61326c49815c272767fb472798c1342f2a
gazjoy <post@garethjoyce.co.uk>
2020-05-05 22:52:38 +0100

    [Matching] adds dob check and reorder journey numbers

--- app/views/2/b/_routes.js
+++ app/views/2/b/_routes.js
@@ -4,6 +4,20 @@ var router = express.Router()
 const BASE_PATH = '/';
 const ABS_BASE_PATH = `${BASE_PATH}`;
 
+router.post('/nino', function (req, res) {
+    const answer = req.session.data['nino'],
+    match = ['QQ 12 34 56 C', 'QQ123456C', 'qq 12 34 56 c', 'qq123456c'];
+    dupe = ['duplicate', 'dupe', 'dup', 'QQ 12 34 56 D', 'qq 12 34 56 d', 'QQ123456D', 'qq123456d'];
+
+    if (match.includes(answer)) {
+        res.redirect('check');
+    } else if (dupe.includes(answer)) {
+        res.redirect('call');
+    } else {
+        res.redirect('processing');
+    }
+})
+
 router.post('/confirm', function (req, res) {
     const answer = req.session.data['nino'],
     match = ['QQ 12 34 56 C', 'QQ123456C', 'qq 12 34 56 c', 'qq123456c'];
@@ -11,7 +25,18 @@ router.post('/confirm', function (req, res) {
     if (match.includes(answer)) {
         res.redirect('check');
     } else {
-        res.redirect('../a/processing');
+        res.redirect('processing');
+    }
+})
+
+router.post('/check', function (req, res) {
+    const answer = req.session.data['matching-details'];
+    if (answer == 'correct') {
+        res.redirect('problem')
+    } else if (answer == 'nino') {
+        res.redirect('nino')
+    } else {
+        res.redirect('update-personal-details')
     }
 })
 
